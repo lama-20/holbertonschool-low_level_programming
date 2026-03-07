e2cb4c48f18f1383f42367080793f3aac5ea9ec3
lama <lama72316@gmail.com>
2026-03-07 17:47:04 +0000

    Add missing task 4 print_rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
  * print_rev - prints a string, in reverse, followed by a new line.
- * @s: the string to be printed in reverse.
+ * @s: string to be printed.
  *
  * Return: void
  */
@@ -15,10 +15,10 @@ void print_rev(char *s)
 	{
 		len++;
 	}
+
 	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
-
 	_putchar('\n');
 }
