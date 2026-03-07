39352ad2f4537bddefcefca3104ca8a5a85932e7
lama <lama72316@gmail.com>
2026-03-07 17:29:43 +0000

    Finish task 1: swap_int

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: first integer to swap.
+ * @b: second integer to swap.
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
