04981a90cd45a35f7ea84de5e9b2342e2c34bd85
lama <lama72316@gmail.com>
2026-04-04 18:25:04 +0000

    Task 0. Float like a butterfly, sting like a bee

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+    char *ptr;
+    unsigned int i;
+
+    if (size == 0)
+    {
+        return (NULL);
+    }
+
+    ptr = malloc(sizeof(char) * size);
+
+    if (ptr == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        ptr[i] = c;
+    }
+
+    return (ptr);
+}
