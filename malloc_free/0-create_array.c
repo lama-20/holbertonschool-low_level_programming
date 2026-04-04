58703d71185447ba5a73d51f63ebd4ec222ffe5a
lama <lama72316@gmail.com>
2026-04-04 18:34:33 +0000

    Fix all Betty style and documentation errors

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,25 +3,25 @@
 
 char *create_array(unsigned int size, char c)
 {
-    char *ptr;
-    unsigned int i;
+	char *ptr;
+	unsigned int i;
 
-    if (size == 0)
-    {
-        return (NULL);
-    }
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
-    ptr = malloc(sizeof(char) * size);
+	ptr = malloc(sizeof(char) * size);
 
-    if (ptr == NULL)
-    {
-        return (NULL);
-    }
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        ptr[i] = c;
-    }
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
 
-    return (ptr);
+	return (ptr);
 }
